52096f7a1cc176a2683a2864fca3da2fad46620a
Svetoslav <svetoslav.atanasov97@gmail.com>
2020-11-25 02:08:10 +0100

    Added Color Picker

--- Assets/Scripts/CircleSlider.cs
+++ Assets/Scripts/CircleSlider.cs
@@ -6,32 +6,54 @@ using UnityEngine.EventSystems;
 
 public class CircleSlider : MonoBehaviour
 {
-    public EventTrigger eventTrigger;
-    public Transform handle;
-    public Image slider;
+    //public EventTrigger eventTrigger;
+    //public GameObject handle;
+    //public Image slider;
 
-    Vector3 mousePos;
+    public Image colorPreview;
+    public Image colorWheel;
 
+    private Color color;
     private void Start()
     {
-        eventTrigger = GetComponentInChildren<EventTrigger>();
-
+        //eventTrigger = GetComponentInChildren<EventTrigger>();
     }
 
-    public void OnHandleDrag()
+    private void Update()
     {
-        Screen.sleepTimeout = SleepTimeout.NeverSleep;
-        mousePos = Input.mousePosition;
+        if (Input.GetMouseButtonDown(0))
+        {
+            Texture2D tex = colorWheel.sprite.texture;
 
-        Vector2 dir = mousePos - handle.position;
-        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            Rect r = colorWheel.rectTransform.rect;
+            Vector2 localPoint;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(colorWheel.rectTransform, Input.mousePosition, null, out localPoint);
 
-        Debug.Log(angle);
-        Quaternion r = Quaternion.AngleAxis(angle + 90f, Vector3.forward);
+            int px = Mathf.Clamp(0, (int)(((localPoint.x - r.x) * tex.width) / r.width), tex.width);
+            int py = Mathf.Clamp(0, (int)(((localPoint.y - r.y) * tex.height) / r.height), tex.height);
 
-        float amount = ((360 - (angle + 90f)) % 360) / 360;
+            Color col = tex.GetPixel(px, py);
 
-        handle.rotation = r;
-        slider.fillAmount = amount;
+            color = new Color(col.r, col.g, col.b);
+            colorPreview.color = color;
+            Debug.Log(col);
+        }
     }
+
+    //public void OnHandleDrag()
+    //{
+    //    Screen.sleepTimeout = SleepTimeout.NeverSleep;
+    //    mousePos = Input.mousePosition;
+
+    //    Vector2 dir = mousePos - handle.position;
+    //    float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+
+    //    Debug.Log(angle);
+    //    Quaternion r = Quaternion.AngleAxis(angle + 90f, Vector3.forward);
+
+    //    float amount = ((360 - (angle + 90f)) % 360) / 360;
+
+    //    handle.rotation = r;
+    //    slider.fillAmount = amount;
+    //}
 }
